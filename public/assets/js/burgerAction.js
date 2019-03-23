d5b8443a6fbe198cce3bbc82bc80db951567b49c
Taylor F Stiger <taylorfgomez@gmail.com>
2019-03-22 23:44:36 -0500

    working commit

---
+++ public/assets/js/burgerAction.js
@@ -0,0 +1,40 @@
+$(function () {
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("newburger")
+                .val()
+                .trim(),
+            devoured: 0
+        };
+
+        $.ajax("/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function () {
+            console.log("Added a new burger");
+            location.reload();
+        });
+    });
+
+    $(".eatburger").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+
+        $.ajax("/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function () {
+            console.log("Burger Devoured");
+            location.reload();
+        });
+    });
+});
+
+
